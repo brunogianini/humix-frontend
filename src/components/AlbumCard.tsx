14a23c6c14c12244b4e821dbe2676d5c9d0b66c5
Bruno Gianini <blgianini@gmail.com>
2024-12-23 08:13:51 -0300

    fix album card

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -10,7 +10,7 @@ export default function AlbumCard({ ...props }: Album) {
                     <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
                     <div className="absolute bottom-0 left-0 right-0 p-4">
                         <h3 className="text-xl font-bold text-white mb-1 line-clamp-1">{props.nome}</h3>
-                        <p className="text-sm text-gray-200 line-clamp-1">a</p>
+                        <p className="text-sm text-gray-200 line-clamp-1">{props.banda.nome}</p>
                     </div>
                 </div>
 
