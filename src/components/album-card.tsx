98e38c8277a008a463aa5f96a77bc2b7935f6858
Bruno Gianini <blgianini@gmail.com>
2025-06-26 23:59:05 -0300

    lapidando layout card

--- src/components/album-card.tsx
+++ src/components/album-card.tsx
@@ -6,8 +6,8 @@ interface AlbumProps {
 
 export function AlbumCard({...props}: AlbumProps){
     return(
-        <div className="relative w-60 rounded-2xl">
-            <img className="rounded-2xl" src={props.capa} />
+        <div className="relative w-60 h-60 rounded-2xl">
+            <img className="rounded-2xl w-60 h-60" src={props.capa} />
             
             <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70 rounded-b-2xl">
                 <h3 className="text-xl text-white font-bold">{props.nome}</h3>
