1fee6b2fdc5e6b0c85a97ea42476786eddf4f2cc
Bruno Gianini <blgianini@gmail.com>
2025-06-26 22:03:49 -0300

    sidebar start

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  Music,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Humix",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    }
+  ],
+  navMain: [
+    {
+      title: "Albums",
+      url: "#",
+      icon: Music,
+      isActive: true,
+      items: [
+        {
+          title: "Meus Álbums",
+          url: "#",
+        },
+        {
+          title: "Não Ouvidos",
+          url: "#",
+        },
+        {
+          title: "Recomendados",
+          url: "#",
+        },
+      ],
+    },
+  ]
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
