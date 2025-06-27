ddc3922488781b3de270e32258587f16be71638c
Bruno Gianini <blgianini@gmail.com>
2025-06-27 00:09:57 -0300

    truncando o texto do album card

--- src/components/album-card.tsx
+++ src/components/album-card.tsx
@@ -10,7 +10,7 @@ export function AlbumCard({...props}: AlbumProps){
             <img className="rounded-2xl w-60 h-60" src={props.capa} />
             
             <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70 rounded-b-2xl">
-                <h3 className="text-xl text-white font-bold">{props.nome}</h3>
+                <h3 className="text-xl text-white font-bold truncate">{props.nome}</h3>
                 <p className="mt-2 text-sm text-gray-300">{props.banda}</p>
             </div>
         </div>
