c8b319d8d49c4041dda539f610208f7f755b9209
Bruno Gianini <blgianini@gmail.com>
2025-06-29 16:42:57 -0300

    fix: mudando a url da api

---
+++ src/components/bucar-usuario-grid.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import UserCard from "./user-card"
+import { useSession } from "next-auth/react"
+import { getUserIdFromSession } from "@/lib/get-user-id-from-session"
+
+interface UsuarioProps {
+    id: string
+    nome: string
+    foto: string
+    isFollower: boolean
+}
+
+export default function BuscarUsuarioGrid(){
+    const { data: session } = useSession()
+    const [users, setUsers] = useState([])
+
+    const userId = getUserIdFromSession(session)
+
+    async function getUsers(){
+        const res = await fetch("https://bumpy-unicorn-brunogianini-8376417b.koyeb.app/usuarios/" + userId)
+        const data = await res.json()
+
+        setUsers(data.usuarios)
+    }
+
+    useEffect(() => {
+        if (userId) getUsers()
+    }, [session])
+
+    return(
+        <main className="flex m-5 gap-5">
+            {users.map((usuario: UsuarioProps, key) => (
+                <UserCard userId={userId ?? ""} onUserFollow={getUsers} key={usuario.id} id={usuario.id} nome={usuario.nome} foto={"https://img.freepik.com/vetores-premium/icone-de-perfil-de-avatar-padrao-imagem-de-usuario-de-midia-social-icone-de-avatar-cinza-silhueta-de-perfil-em-branco-ilustracao-vetorial_561158-3383.jpg?semt=ais_hybrid&w=740"} seguindo={false} me_segue={usuario.isFollower} />
+            ))}
+        </main>
+    )
+}
---
+++ src/components/user-card.tsx
@@ -0,0 +1,42 @@
+import { Badge } from "./ui/badge"
+import { Button } from "./ui/button"
+
+interface UsuarioProps {
+    userId: string
+    id: string
+    nome: string
+    foto: string
+    seguindo: boolean
+    me_segue: boolean
+    onUserFollow: () => void
+}
+
+export default function UserCard({...props}: UsuarioProps){
+    async function seguirUsuario(){
+        let bodyContent = JSON.stringify({
+            "userId": props.userId,
+            "alvoId": props.id
+        })
+
+        const seguir = await fetch('https://bumpy-unicorn-brunogianini-8376417b.koyeb.app/seguir',{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: bodyContent
+        })
+        const data = await seguir.json()
+        if (props.onUserFollow) props.onUserFollow()
+    }
+    return(
+        <div className="h-25 w-80 rounded-2xl border-2 flex items-center p-2 gap-5">
+            <img className="h-20" src={props.foto} alt="" />
+            <div>
+                {props.nome}
+                {props.me_segue? <Badge>Te segue</Badge> : ""}
+            </div>
+            {props.seguindo ? "" : <Button className="cursor-pointer" onClick={seguirUsuario}>Seguir</Button>}
+            
+        </div>
+    )
+}
