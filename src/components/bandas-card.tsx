730dd0f785d29e9283630f576fe463b37938bc8c
Bruno Gianini <blgianini@gmail.com>
2025-06-29 01:01:33 -0300

    media de notas

--- src/components/bandas-card.tsx
+++ src/components/bandas-card.tsx
@@ -1,20 +1,51 @@
-interface bandasProps{
-    nome: string
-    foto: string
-    albums: []
+'use client'
+
+import { useEffect, useState } from "react"
+
+interface Avaliacao {
+    // Define properties of Avaliacao as needed, using 'any' if unknown
+    [key: string]: any;
 }
 
 interface albumProps{
-    capa: string
+    capa: string;
+    avaliacoes: Avaliacao[];
+}
+
+interface bandasProps{
+    nome: string;
+    foto: string;
+    albums: albumProps[];
 }
 
 export default function BandasCard({...props}: bandasProps){
+    const [media, setMedia] = useState(0)
+
+    function calcularMedia(){
+        let soma = 0
+        let n = props.albums.length
+        props.albums.forEach(album => {
+            
+            album.avaliacoes.forEach((avaliacao: any) => {
+                soma += avaliacao.nota
+            })
+        })
+
+        setMedia(soma / n)
+    }
+
+    useEffect(() => {
+        calcularMedia()
+    }, [])
+
+    
     return(
         <div className="w-ful border h-25 rounded-2xl flex items-center p-3 gap- justify-between">
 
             <div className="flex items-center gap-5">
                 <img className="h-20" src={props.foto} alt="" />
                 {props.nome}
+                <p className="ml-5">média de notas: {media}</p>
             </div>
             
             <div className="flex gap-5">
